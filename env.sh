5e984249f777f3f8cac4a581e4c407053952bbf4
Xiaoshuai Zhang <jetgabr@gmail.com>
2024-06-25 15:31:51 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 sudo apt install libglm-dev
 ulimit -n 8192
-conda create -n sgs python=3.12
+conda create -n sgs python=3.11
 conda activate sgs
+pip install torch==2.3.1 torchvision==0.18.1
 pip install -r requirements.txt
 pip install ./submodules/diff_gaussian_rasterization_softmax
 pip install ./submodules/simple_knn
